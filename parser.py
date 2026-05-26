356d9f88cf86538e6cfab2740694e9aaf04e7463
AlexSauv <141260346+AlexSauv@users.noreply.github.com>
2026-05-26 15:56:12 +0200

    trying to separate meatadata from content

--- parser.py
+++ parser.py
@@ -6,7 +6,7 @@ class MapParser:
     def __init__(self, name_file: str):
         self.name_file = name_file
         self.nb_drones: int = 0
-        self.hubs: dict[str, Hub] = []
+        self.hubs: dict[str, Hub] = {}
         self.connections: list[Connection] = []
         self.start_hub: Optional[Hub] = None
         self.end_hub: Optional[Hub] = None
@@ -19,57 +19,84 @@ class MapParser:
                     cleaned_line = line.strip()
                     if cleaned_line and not cleaned_line.startswith("#"):
                         settings.append(cleaned_line)
+                    if cleaned_line and cleaned_line.find(":") == -1:
+                        raise ValueError
         except OSError:
             print("[Error] There are issues with the settings file.")
             sys.exit(1)
-
-        if not settings[0].startswith("nb_drones:"):
+        except ValueError:
+            print("[Error] The format is key:value [optional=detail]")
+            sys.exit(1)
+        try:
+            if settings[0].startswith("nb_drones:"):
+                drones = int(settings[0].split(":")[1].strip())
+                if drones < 0:
+                    raise ValueError
+                self.nb_drones = drones
+  
+        except ValueError:
             print("[Error] The settings file must begin with "
                   "nb_drones: (integer).")
             sys.exit(1)
+
         if not settings:
             print("[Error] The settings file must contain datas.")
             sys.exit(1)
         return settings
 
-    def split_keys_values(self):
+    def fetch_hub_datas(self):
         datas = self.fetch_infos()
-        keys_values: dict = {}
         for data in datas:
-            separator = data.find(":")
-            if separator != -1 and data.count(':', 1):
-                key, value = data.split(":")
-                if key in keys_values:
-                    keys_values[key].update({value.strip()})
-                else:
-                    keys_values.setdefault(key, {value.strip()})
-            else:
-                print("[Error] There are issues with the settings file.")
-                print("[Error] The format is key:value [optional=detail]")
-                sys.exit(1)
-        return keys_values
+            i = 0
+            prefix, details = data.split(":")
+            metadata = self.fetch_metadata(details)
+            if "hub" in prefix:
+                content: list = details.split()
+                for meta in content:
+                    if metadata and meta == metadata:
+                        content.pop(i)
+                    i += 1
+                # metadata = [meta for meta in content if "[" in meta]
+                # metadata += [meta for meta in content if "]" in meta]
+                print(prefix)
+                print(content)
+                print(f"THAT'S META_DATA {metadata}")
 
-    def fetch_metadata(self):
-        settings = self.fetch_infos()
-        for key in settings:
-            optional = settings[key].strip('[]')
-            options = []
-            special = []
-            if optional:
-                options.append(optional.split(" "))
-                for _ in options:
-                    special.append(options.split("="))
-                    if special > 1:
-                        settings[key].setdefault("option",
-                                                 {special[0]: special[1]})
+        # return keys_values
+    
+    # def fetch_hub_datas(self):
+    #     raw_hubs = self.fetch_infos()
 
+    #     # datas = self.split_keys_values()
+    #     # for data in datas:
+    #     #     if data == "nb_drones":
+    #     #         drones = datas[data]
+    #     #         self.nb_drones = int(drones)
+    #     #     elif data == "start_hub":
+    #     #         name = datas[data].endswith(" ")
+                
+
+    def fetch_metadata(self, details: list):
+        meta_data: str | None = ""
+        if "[" and "]" in details:
+            meta_data_start = details.index('[')
+            meta_data_end = details.index(']')
+            while meta_data_start < meta_data_end + 1:
+                meta_data += details[meta_data_start]
+                meta_data_start += 1
+        if len(meta_data) <= 1:
+            meta_data = None
+        return meta_data
+        
             
 
 
 def main() -> None:
     settings = MapParser('config.txt')
-    test = settings.split_keys_values()
-    print(f"Key {test} ==> {test.keys()}")
+    settings.fetch_hub_datas()
+    # print(f"Key {test} ==> {test}")
+    # print(settings.nb_drones)
+    # settings.fetch_hub_datas()
 
 
 if __name__ == "__main__":
