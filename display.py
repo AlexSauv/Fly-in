47a0b50b2138f3949c3d5b0c1a96476739348e4e
alsauvan <alsauvan@student.42lyon.fr>
2026-06-19 13:43:16 +0200

    fixing rendering

--- display.py
+++ display.py
@@ -11,7 +11,7 @@ class MapDisplay(arcade.Window):
         self.simulation = simulation
         self.map_fly = simulation.map_fly
         arcade.set_background_color(arcade.color.DARK_BLUE)
-        self.drone_txt = arcade.load_texture("drone_one.png")
+        self.drone_txt = arcade.load_texture("drone_img.png")
         self.drone_sprites = arcade.SpriteList()
         self.drone_speed = 5
         self.set_drone()
@@ -23,7 +23,7 @@ class MapDisplay(arcade.Window):
         for hub in hubs.values():
             if hub.drones:
                 for drone_data in hub.drones:
-                    drone_sprite = arcade.Sprite(self.drone_txt, scale=0.015)
+                    drone_sprite = arcade.Sprite(self.drone_txt, scale=0.09)
                     drone_sprite.center_x = hub.position[0] * 150 + center_x
                     drone_sprite.center_y = hub.position[1] * 150 + center_y
                     drone_sprite.target_x = drone_sprite.center_x
@@ -80,14 +80,14 @@ class MapDisplay(arcade.Window):
                 arcade.draw_circle_filled(x, y, 35, color_rgb)
             else:
                 raise ValueError(f"[DISPLAY] {color} color not found")
-            hub_name = arcade.Text(hubs[hub].name, x, y - 5,
-                                   arcade.color.BLACK, font_size=8)
+            hub_name = arcade.Text(hubs[hub].name, x - 15, y - 45,
+                                   arcade.color.WHITE, font_size=10)
             hub_name.draw()
         self.drone_sprites.draw()
         output = f"Total Turns: {self.simulation.turn}"
         arcade.draw_text(output, 10, 20, arcade.color.WHITE, 14)
 
-    def on_update(self, time):
+    def on_update(self):
         drones_arrived = True
         center_x, center_y = self.get_center()
         for drone in self.drone_sprites:
@@ -125,8 +125,6 @@ if __name__ == "__main__":
 
         simulation = Manager(map_fly)
         simulation.initiate_simulation()
-        # while simulation.simulation_turn():
-        #     pass
         renderer = MapDisplay(1920, 1080, "Fly-in", simulation)
         arcade.run()
         print(simulation.turn)
